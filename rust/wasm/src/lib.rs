1ae39b98649f1c51f9c650ed24fd1c7e51951b62
Piers Finlayson <piers@piers.rocks>
2025-10-05 12:02:05 +0100

    cargo fmt

--- rust/wasm/src/lib.rs
+++ rust/wasm/src/lib.rs
@@ -2,10 +2,9 @@
 //
 // MIT License
 
-/// Web Assembly bindings for One ROM supporting tooling.
-
-use wasm_bindgen::prelude::*;
 use sdrr_fw_parser::{Parser, readers::MemoryReader};
+/// Web Assembly bindings for One ROM supporting tooling.
+use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(start)]
 pub fn init() {
@@ -15,14 +14,17 @@ pub fn init() {
 
 #[wasm_bindgen]
 pub async fn parse_firmware(data: Vec<u8>) -> Result<JsValue, JsValue> {
+    // We hard-code 0x08000000 as the base address, but the parser
+    // automaticaly detects if it's looking at an RP2350 firmware, and adjusts
+    // the base address dynamically.
     let mut reader = MemoryReader::new(data, 0x08000000);
     let mut parser = Parser::new(&mut reader);
-    
-    let info = parser.parse_flash()
+
+    let info = parser
+        .parse_flash()
         .await
         .map_err(|e| JsValue::from_str(&e))?;
-    
+
     // Serialize to JSON
-    serde_wasm_bindgen::to_value(&info)
-        .map_err(|e| JsValue::from_str(&e.to_string()))
+    serde_wasm_bindgen::to_value(&info).map_err(|e| JsValue::from_str(&e.to_string()))
 }
