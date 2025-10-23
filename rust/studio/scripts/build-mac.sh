058ad20043b7432aa9a8931b9e91f2d0c6913072
Piers Finlayson <piers@piers.rocks>
2025-10-23 11:21:22 +0100

    craft single mac build script

---
+++ rust/studio/scripts/build-mac.sh
@@ -0,0 +1,91 @@
+#!/bin/sh
+set -e
+
+# Builds the Rust Studio application and dmg packages for macOS.
+#
+# Pre-requisites:
+# - Rust:
+#
+# ```zsh
+#   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# ```
+#
+# - Homebrew:
+#
+# ```zsh
+#   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# ```
+#
+# - Python 3 and pip: https://www.python.org/downloads/macos/
+
+# Check it's running on macOS
+if [ "$(uname -s)" != "Darwin" ]; then
+    echo "Error: This script must be run on macOS" >&2
+    exit 1
+fi
+
+#
+# Setup
+#
+
+# Set libusb to static linking
+export LIBUSB_STATIC=1
+
+# Install the Rust targets
+rustup target add x86_64-apple-darwin
+rustup target add aarch64-apple-darwin
+
+# Install cargo-packager if not already installed
+cargo install cargo-packager --locked
+
+# Install fileicon if not already installed
+brew install fileicon
+
+# Install python pip packages
+python3 -m pip install --break-system-packages -r scripts/requirements.txt
+
+#
+# Clean previous builds
+#
+
+cargo clean --target x86_64-apple-darwin
+cargo clean --target aarch64-apple-darwin
+rm -fr dist/
+
+#
+# Intel silicon (x86_64)
+#
+
+# Build One ROM Studio
+PACKAGER_TARGET="x86_64-apple-darwin"
+echo "Building for target: $PACKAGER_TARGET"
+cargo build --release --target $PACKAGER_TARGET
+
+# Package as a dmg
+cargo packager --release --target $PACKAGER_TARGET --formats dmg
+
+# Update the dmg background, icon positions and volume icon
+scripts/update-dmg.py
+
+# Clean intermediate dmg and Cargo build
+cargo clean --target x86_64-apple-darwin
+rm dist/*_cargo.dmg
+
+#
+# Apple Silicon (aarch64)
+#
+
+# Build One ROM Studio
+PACKAGER_TARGET="aarch64-apple-darwin"
+echo "Building for target: $PACKAGER_TARGET"
+cargo build --release --target $PACKAGER_TARGET
+
+# Package as a dmg
+cargo packager --release --target $PACKAGER_TARGET --formats dmg
+
+# Update the dmg background, icon positions and volume icon
+scripts/update-dmg.py
+
+# Clean intermediate dmg and Cargo build
+cargo clean --target aarch64-apple-darwin
+rm dist/*_cargo.dmg
