8ffbaa8cae7b332c81d6f7706914bb90f17ab87b
Piers Finlayson <piers@piers.rocks>
2025-10-23 13:01:04 +0100

    get win script working

--- rust/studio/scripts/build-win.ps1
+++ rust/studio/scripts/build-win.ps1
@@ -37,10 +37,10 @@ Remove-Item -Path "dist\*.exe" -Force -ErrorAction SilentlyContinue
 # Build One ROM Studio
 $env:PACKAGER_TARGET = "x86_64-pc-windows-msvc"
 Write-Host "Building for target: $env:PACKAGER_TARGET"
-cargo build --release --target $env:PACKAGER_TARGET
+cargo build --release --target $env:PACKAGER_TARGET | Out-Host
 
 # Package as NSIS installer
 Write-Host "Packaging NSIS installer for target: $env:PACKAGER_TARGET"
-cargo packager --release --target $env:PACKAGER_TARGET --formats nsis
+cargo packager --release --target $env:PACKAGER_TARGET --formats nsis | Out-Host
 
 Write-Host "Windows x86_64 build complete."
