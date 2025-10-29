a5fab3623895298d1c6a5521f419c70f9c390133
Piers Finlayson <piers@piers.rocks>
2025-10-29 10:08:06 +0000

    update mac scripts

--- rust/studio/scripts/build-mac.sh
+++ rust/studio/scripts/build-mac.sh
@@ -1,28 +1,30 @@
 #!/bin/sh
 set -e
 
-# Builds the One ROM Studio application and dmg packages for macOS.
+# Builds One ROM Studio for macOS, with a single binary for both architectures
 #
-# Designed to be run from CI.
+# Required env variables:
+# - CODESIGN_IDENTITY: The codesign identity to use for signing the app.
+#   This is the "name" of the certificate including "Developer ID ...".
+#   Only required if not building with "nosign" option.
 #
-# DOES NOT SIGN OR NOTARIZE THE BUILDS.
-#
-# To run with code signing and notarization, use build-mac-arch.sh directly.
-#
-# Pre-requisites:
-# - Rust:
-#
-# ```sh
-#   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-# ```
-#
-# - Homebrew:
+# The notarytool uses a keychain profile named "onerom-notarization" for
+# notarization. This profile must be set up in advance with the appropriate
+# Apple ID and app-specific password using a command line:
 #
 # ```sh
-#   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# xcrun notarytool store-credentials "onerom-notarization" \
+#  --apple-id your@email.com \
+#  --team-id XXXXXXXXXX \
+#  --password xxxx-xxxx-xxxx-xxxx
 # ```
 #
-# - Python 3 and pip: https://www.python.org/downloads/macos/
+# - Team ID matches that on the Developer ID Application certificate.
+# - The app-specific password is generated in the Apple ID account settings.
+
+#
+# Checks
+#
 
 # Check we're running on macOS
 if [ "$(uname -s)" != "Darwin" ]; then
@@ -30,11 +32,39 @@ if [ "$(uname -s)" != "Darwin" ]; then
     exit 1
 fi
 
+# Set signing mode
+SIGN="$2"
+if [ "$SIGN" = "nosign" ]; then
+    echo "!!! WARNING: Building without code signing or notarization" >&2
+    CODESIGN_IDENTITY=""
+else
+    # Check codesign identity is set
+    if [ -z "${CODESIGN_IDENTITY:-}" ]; then
+        echo "Error: CODESIGN_IDENTITY environment variable must be set"
+        exit 1
+    fi
+fi
+
 #
-# Setup
+# Variable
+#
+
+# Distribution directory - DMG goes here
+DIST_DIR="dist"
+
+# Get version from Cargo.toml
+VERSION=$(grep "^version" Cargo.toml | head -1 | sed 's/.*"\(.*\)".*/\1/')
+
+# Set DMG filename
+DMG_FILE="One ROM Studio_${VERSION}.dmg"
+DMG_PATH="${DIST_DIR}/${DMG_FILE}"
+
+#
+# Dependencies
 #
 
 # Install the Rust targets
+echo "Installing Rust targets..."
 rustup target add x86_64-apple-darwin
 rustup target add aarch64-apple-darwin
 
@@ -51,12 +81,86 @@ python3 -m pip install --break-system-packages -r scripts/requirements.txt
 # Clean previous builds
 #
 
-cargo clean --target x86_64-apple-darwin
-cargo clean --target aarch64-apple-darwin
-rm -fr dist/*.dmg
+echo "Cleaning previous build artifacts..."
 
-# Build for x86_64
-scripts/build-mac-arch.sh x86_64 nosign
+# Do a Cargo clean
+cargo clean
 
-# Build for aarch64
-scripts/build-mac-arch.sh aarch64 nosign
+# Delete old DMG
+if [ -f "${DMG_PATH}" ]; then
+    echo "Removing old dmg: ${DMG_PATH}"
+    rm -f "${DMG_PATH}"
+fi
+
+#
+# Build
+#
+
+echo "Building One ROM Studio version: $VERSION"
+echo "Building dmg: $DMG_PATH"
+
+# Build for Intel
+PACKAGER_TARGET="x86_64-apple-darwin"
+echo "Building One ROM Studio for target: $PACKAGER_TARGET"
+cargo build --release --target $PACKAGER_TARGET
+
+# Build for ARM64
+PACKAGER_TARGET="aarch64-apple-darwin"
+echo "Building One ROM Studio for target: $PACKAGER_TARGET"
+cargo build --release --target $PACKAGER_TARGET
+
+# Package as a .app bundle - do this using ARM64 target for now
+echo "Bundling dmg for target: $PACKAGER_TARGET"
+cargo bundle --release --target $PACKAGER_TARGET
+APP_FILE="../target/$PACKAGER_TARGET/release/bundle/osx/One ROM Studio.app"
+echo "Built app file: $APP_FILE"
+
+# Replace App binary with a universal binary
+lipo -create \
+  ../target/x86_64-apple-darwin/release/onerom-studio \
+  ../target/aarch64-apple-darwin/release/onerom-studio \
+  -output "$APP_FILE/Contents/MacOS/onerom-studio"
+
+# Check if signing is enabled
+if [ -n "$CODESIGN_IDENTITY" ]; then
+    # Sign the app
+    echo "Signing app..."
+    codesign --deep --verify --options runtime --timestamp --sign "$CODESIGN_IDENTITY" "$APP_FILE"
+
+    # Verify the signature
+    echo "Verifying signature..."
+    codesign -dvv "$APP_FILE" 2>&1 | grep "Authority=Developer ID Application"
+    if [ $? -ne 0 ]; then
+        echo "Error: App signature verification failed" >&2
+        exit 1
+    fi
+fi
+
+# Create the dmg
+echo "Creating dmg..."
+scripts/create-dmg.py \
+    --app-bundle "$APP_FILE" \
+    --output "${DMG_FILE}" \
+    --dist-dir ${DIST_DIR}
+
+# Check if signing is enabled
+if [ -n "$CODESIGN_IDENTITY" ]; then
+    # Notarize the dmg
+    echo "Notarizing dmg..."
+    xcrun notarytool submit "$DMG_PATH" \
+        --keychain-profile "onerom-notarization" \
+        --wait
+
+    # Staple the notarization ticket to the dmg
+    echo "Stapling dmg..."
+    xcrun stapler staple "$DMG_PATH"
+
+    # Validate the stapled ticket
+    echo "Validating stapled ticket..."
+    xcrun stapler validate "$DMG_PATH"
+
+    # Finished
+    echo "Built and notarized dmg: $DMG_PATH"
+else
+    echo "Built dmg without notarization: $DMG_PATH"
+fi
