e9e33e2523d2159c1959d9fa2981cd4e570c4986
Piers Finlayson <piers@piers.rocks>
2025-11-17 11:20:39 +0000

    Improve comments

--- sdrr/src/piorom.c
+++ sdrr/src/piorom.c
@@ -9,10 +9,14 @@
 
 #if defined(RP_PIO)
 
+// # Introduction
+
 // This file contains a completely autonomous PIO and DMA based ROM serving
 // implementation.  Once started, the PIO state machines and DMA channels
 // serve ROM data in response to external chip select and address lines
 // without any further CPU intervention.
+//
+// # Algorithm Summary
 //
 // The implementation uses three PIO state machines and 2 DMA channels, with
 // the following overall operation:
@@ -37,6 +41,77 @@
 //     |                                                            v
 //     <-------------------------------------------------------------
 //                                                   (Not to scale)
+//
+// # Timings
+//
+// It is difficult to be sure, but based on observed data, and theoretical
+// estimates, the timings are estimated as follows:
+// - Address valid to correct data byte is 11-14 cycles
+// - Previous data valid after address change delay 14-11 cycles (although
+//   it is much less than this is CS is made inactive, which is very likely)
+// - CS active to data output is 5-6 cycles
+// - CS inactive to data inputs is 3 cycles
+//
+// Physical settling time of lines will add to this.  Also, experience has
+// shown that the system is likely to introduce other, unplanned for, stalls
+// and other delays.  In particular if running _anything_ else, such as having
+// an SWD debug probe connected, may introduce delays and jitter due to bus
+// contention.
+//
+// At a max rated RP2350 clock speed of 150MHz this is:
+// - 73-93ns from address to data
+// - 33-40ns from CS active to data output
+// - 20ns from CS inactive to data inputs
+//
+// At 50MHz:
+// - 220-280ns from address to data
+// - 280-220ns from previous data valid after address change
+// - 100-120ns from CS active to data output
+// - 60ns from CS inactive to data inputs
+//
+// Overclocked to 300MHz:
+// - 37-47ns from address to data
+// - 17-20ns from CS active to data output
+// - 10ns from CS inactive to data inputs
+//
+// Address to data breakdown:
+// - 2 cycle delay in GPIO state reaching PIO due to input-sync
+// - SM1 address read 3-4 cycles:
+//   - 3 is best case scenario
+//   - 6 is worst case, but this "swallows" the input-sync delay, leading to 4
+// - Triggering DMA via DREQ from SM1 RX FIFO 1 cycle
+// - DMAs take 2-3 cycles each:
+//   - 3 cycles is likely due to single cycle stall due to contention, likely
+//     with other DMA channel.
+//   - Assume no stall in transfer between them.
+// - SM2 data byte output 1 cycle
+//
+// Previous data valid after address change breakdown:
+// - Inverse of address to data breakdown
+//
+// CS active to data output breakdown:
+// - 2 cycle delay in GPIO state reaching PIO due to input-sync
+// - SM0 best case is 3 cycles - mov x, pins; jmp x--, N; mov pindirs, ~null
+// - SM0 worst case adds 3 cycles, 2 of which "swallow" the input-sync delay
+//
+// CS active to inactive breakdown:
+// - 2 cycle delay in GPIO state reaching PIO due to input-sync
+// - SM0 best case is 3 cycles - mov x, pins; jmp !x, N; mov pindirs, null
+// - SM0 worst case add 2 cycles, but these "swallow" the input-sync delay
+//
+// These timings do not quite add up.  The C64 character ROM is a 2332A, with
+// 350ns access time - the maximum time allowed to go from address valid to
+// valid.  As we can serve this ROM successfully at around 50MHz - with our
+// worse cast estimate of 280ns for this time - either our estimates are wrong,
+// or the C64 VIC-II requires better of the ROM than its specification - or
+// both.  Worst case it seems like our estimates may be 20% under (i.e add 25%
+// to them).
+// 
+// Therefore 50ns operation may require the RP2350 to be clocked closer to
+// 400MHz than 300Mhz.  This is still likely to be within the RP2350's
+// capabilities.
+//
+// # Detailed Operation
 //
 // The detailed operation is as follows:
 //
@@ -112,14 +187,18 @@
 // - May need to add delays, e.g. before reading address lines to allow them
 //   stabilise.  Ideally, we would make side-set delays configurable at
 //   various points in the algorithm, and include as configuration options.
-
 //
-// Supported PIO configuration options
+// # Supported PIO configuration options
 //
 // Note where min/max clock speeds are given below they tended to vary by
 // 1-2Mhz, based on the day.  Likely due to temperature variations affecting
-// the host's timing.  (It is unlikely the RP2350's timing varies, given it
-// has a modern, extremely accurate, clock source.)
+// the host's timing.  It is unlikely the RP2350's timing varies, given it
+// has a modern, extremely accurate, clock source.
+//
+// For these tests, the RP2350 was not overclocked - the max supported clock
+// speed it known to be higher than 150MHz for these ROMs, but there is a max
+// speed, particularly for character ROMs, due to the video chip requiring a
+// byte to be held after CS is dectivated.
 //
 // # PIO_CONFIG_DEFAULT
 //
@@ -133,6 +212,7 @@
 // Min/Max speeds:
 // - PAL C64 Char ROM: 115-150MHz
 // - PAL C64 Kernal ROM: 45-150MHz
+// - PAL VIC-20 Char ROM: 44-150MHz
 //
 // # PIO_CONFIG_SLOW_CLOCK_KERNAL
 //
@@ -145,6 +225,7 @@
 //
 // Min/Max speeds:
 // - PAL C64 Kernal ROM: 41-150MHz
+// - PAL VIC-20 Kernal ROM: 22-150MHz
 //
 // # PIO_CONFIG_SLOW_CLOCK_CHAR
 //
@@ -157,6 +238,7 @@
 //
 // Min/Max speeds:
 // - PAL C64 Char ROM: 51-150MHz
+// - PAL VIC-20 Char ROM: 51-150MHz
 
 // Fallback default configuration
 #if !defined(PIO_CONFIG_ADDR_READ_IRQ) && !defined(PIO_CONFIG_ADDR_READ_DELAY) && !defined(PIO_CONFIG_CS_TO_DATA_OUTPUT_DELAY) && !defined(PIO_CONFIG_CS_INACTIVE_DATA_HOLD_DELAY)
